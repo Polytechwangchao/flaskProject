120d2f6ad1b56a02cd39f1cbe065f19216d2b9e5
ChaoWANG <nantes1967@gmail.com>
2023-04-12 13:20:47 +0800

    wind_monitor_version1

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template, session
-
-app = Flask(__name__)
+from gevent import pywsgi
+from waitress import serve
+app = Flask(__name__,static_folder='static', static_url_path='',template_folder='templates')
 
 app.secret_key = 'QWERTYUIOP'  # 对用户信息加密
-
+app._static_folder = "templates"
 
 @app.route('/login', methods=["GET","POST"])  # 路由默认接收请求方式为POST，然而登录所需要请求都有，所以要特别声明。
 def login():
@@ -23,4 +24,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app,host="0.0.0.0",port=8080)
